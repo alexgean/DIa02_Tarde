cb46ace6f809763ff5c21b7b46b8c026e6b457e1
Alex <Alex@PC-ALEX>
2022-02-05 18:43:00 -0300

    Adicionar arquivos de projeto.

---
+++ DIa02_Tarde/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DIa02_Tarde.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ArteAbstrata()
+        {
+            var Arq = Server.MapPath("~/Imagens/Imagem.jpg");
+            return base.File(Arq, "Image/jpg");
+        
+        }
+
+        public ActionResult Apostila()
+        {
+            var Arq = Server.MapPath("~/Imagens/PdfUser.pdf");
+            return base.File(Arq, "application/pdf");
+
+        }
+        [HttpGet]
+        public ActionResult ValidaAluno(int Id)
+        {
+            if (Id == 1)
+                return Redirect("/Home/Aluno/1");
+            else
+                return Redirect("/Home/Erro");
+
+        }
+
+        public ActionResult Erro()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+    public string Aluno(int Id)
+        {
+            return "Bem vindo " + Convert.ToString(Id);
+
+        }
+
+        public ActionResult Relatorio()
+        {
+            ViewBag.Cod = "1";
+            ViewBag.Nome = "Rua. Algum Trem";
+            
+            ViewData["Endereco"] = "Rua. Algum Trem";
+            ViewData["CEP"] = "05676-090";
+
+            return View();
+        }
+
+
+        public ActionResult Empresa()
+        {
+            ViewBag.Empresa = "Impacta Tecnologia";
+            ViewData["endereco"] = "Avenida Paulista, 1009";
+            return View();
+        }
+        public string MostrarTexto()
+        {
+            return "<h1>Primeiro exemplo de action</h1>";
+        }
+
+    }
+
+
+
+}
